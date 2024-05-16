06a226ac3776dda1c7b0f000a663066e749254f2
albartol <albartol@student.42madrid.com>
2024-05-16 23:04:06 +0200

    Minor change

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: albartol <albartol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 21:55:59 by albartol          #+#    #+#             */
-/*   Updated: 2024/05/11 00:10:00 by albartol         ###   ########.fr       */
+/*   Updated: 2024/05/16 21:06:38 by albartol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@ int	main(void)
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
 	std::cout << std::setw(5) << "a: " << a << "\n"
+		<< std::setw(5) << "b: " << b << "\n"
+		<< std::setw(5) << "a + b: " << a + b << "\n"
+		<< std::setw(5) << "a - b: " << a - b << "\n"
+		<< std::setw(5) << "a * b: " << a * b << "\n"
+		<< std::setw(5) << "a / b: " << a / b << "\n"
 		<< std::setw(5) << "++a: " << ++a << "\n"
 		<< std::setw(5) << "a: " << a << "\n"
 		<< std::setw(5) << "a++: " << a++ << "\n"
@@ -32,7 +37,6 @@ int	main(void)
 		<< std::setw(5) << "a: " << a << "\n"
 		<< std::setw(5) << "--a: " << --a << "\n"
 		<< std::setw(5) << "a: " << a << "\n"
-		<< std::setw(5) << "b: " << b << "\n"
 		<< "max (a, b): " << Fixed::max( a, b ) << "\n"
 		<< "min (a, b): " << Fixed::min( a, b ) << "\n";
 
